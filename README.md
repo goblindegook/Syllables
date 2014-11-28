7091981c8c2c8d1835ca0b4750d32b3593e640be
Luis Rodrigues <hello@goblindegook.net>
2014-11-28 21:05:36 +0000

    Fragment caching (after Mark Jaquith)

--- README.md
+++ README.md
@@ -15,3 +15,11 @@ TODO
 ### As a must-use WordPress plugin
 
 TODO
+
+## Credits
+
+* `Syllables\Cache\Fragment` is adapted from [Mark Jaquith's fragment caching class](http://markjaquith.wordpress.com/2013/04/26/fragment-caching-in-wordpress/).
+
+## License
+
+Syllables is licensed under the GPL, version 2.0 or any later version. See `LICENSE`.
---
+++ src/Cache/Fragment.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Implement fragment caching.
+ *
+ * @since 0.1.0
+ *
+ * @package Syllables\Cache
+ */
+
+namespace Syllables\Cache;
+
+/**
+ * Class that implements fragment caching.
+ *
+ * Uses code taken from Mark Jaquith's `CWS_Fragment_Cache` class.
+ *
+ * Usage:
+ *
+ * ```
+ * $fragment = new \Syllables\Cache\Fragment( 'unique-key', 3600 );
+ *
+ * if ( ! $fragment->output() ) {
+ *     // Your code goes here.
+ *
+ *     // IMPORTANT! DO NOT FORGET THIS:
+ *     $fragment->store();
+ * }
+ * ```
+ *
+ * @since 0.1.0
+ *
+ * @see http://markjaquith.wordpress.com/2013/04/26/fragment-caching-in-wordpress/
+ */
+class Fragment {
+
+	/**
+	 * Cache group.
+	 */
+	protected $group = 'syllables-cache-fragments';
+
+	/**
+	 * Cache key used to reference cached data.
+	 * @var string
+	 */
+	protected $key;
+
+	/**
+	 * Cached data's time-to-live, in seconds.
+	 * @var int
+	 */
+	protected $ttl;
+
+	/**
+	 * Fragment cache constructor.
+	 *
+	 * @param string $key Key used to reference cached data.
+	 * @param int    $ttl Time-to-live (in seconds).
+	 */
+	public function __construct( $key, $ttl ) {
+		$this->key = $key;
+		$this->ttl = $ttl;
+	}
+
+	/**
+	 * Outputs cached content, if available.
+	 *
+	 * Otherwise, it begins capturing output for caching.
+	 *
+	 * @return boolean Whether content was found in the cache.
+	 */
+	public function output() {
+		$output    = wp_cache_get( $this->key, $this->group );
+		$is_cached = ! empty( $output );
+
+		echo $is_cached ? $output : '';
+
+		if ( ! $is_cached ) {
+			ob_start();
+		}
+
+		return $is_cached;
+	}
+
+	/**
+	 * Stores the rendered snippet in the object cache.
+	 */
+	public function store() {
+		// Flushes the buffers
+		$output = ob_get_flush();
+		wp_cache_add( $this->key, $output, $this->group, $this->ttl );
+	}
+}
